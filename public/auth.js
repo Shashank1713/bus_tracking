32fae14f2bfbe59eadf656827dba822d747a763f
shashank halaki <shashankhalaki@shashanks-MacBook-Air.local>
2026-01-29 19:44:31 +0530

    Added signup UI

--- public/auth.js
+++ public/auth.js
@@ -2,6 +2,32 @@ function togglePassword(){
   password.type = password.type === "password" ? "text" : "password";
 }
 
+function showSignup(){
+  signupBox.style.display="block";
+  title.innerText="Sign Up";
+}
+
+function showSignin(){
+  signupBox.style.display="none";
+  title.innerText="Sign In";
+}
+
+async function signup(){
+  const res = await fetch("/api/signup",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      username:email.value,
+      password:password.value,
+      role:role.value
+    })
+  });
+
+  const data = await res.json();
+  alert(data.message);
+  if(res.ok) showSignin();
+}
+
 async function signin(){
   const res = await fetch("/api/signin",{
     method:"POST",
@@ -15,8 +41,8 @@ async function signin(){
   const data = await res.json();
 
   if(res.ok){
-    if(data.role==="driver") location.href="/driver.html";
     if(data.role==="user") location.href="/user.html";
+    if(data.role==="driver") location.href="/driver.html";
     if(data.role==="admin") location.href="/admin.html";
   } else {
     alert(data.message);
@@ -34,5 +60,6 @@ async function resetPassword(){
       newPassword:newPassword.value
     })
   });
+
   alert((await res.json()).message);
 }
